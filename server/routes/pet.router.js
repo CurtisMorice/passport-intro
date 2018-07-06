252afe9fe5934ac442fe20a4b24eabe15f573001
Curtis Morice <curtismorice@gmail.com>
2018-07-05 19:54:07 -0500

    added if statement to get authentication

--- server/routes/pet.router.js
+++ server/routes/pet.router.js
@@ -7,6 +7,9 @@ router.get('/', (req, res) => {
     console.log('/pet GET route');
     console.log('is authenticated?', req.isAuthenticated());
     console.log('user', req.user);
+
+    if (request.isAuthenticated()){
+
     let queryText = `SELECT * FROM "pet"`;
     pool.query(queryText).then((result) => {
         res.send(result.rows);
@@ -14,15 +17,20 @@ router.get('/', (req, res) => {
         console.log(error);
         res.sendStatus(500);
     });
+}else { res.sendStatus(403);}
 });
 
 // This route *should* add a pet for the logged in user
 router.post('/', (req, res) => {
+    if(req.isAuthenticated()){
     console.log('/pet POST route');
     console.log(req.body);
     console.log('is authenticated?', req.isAuthenticated());
     console.log('user', req.user);
     res.sendStatus(200);
+} else {
+    res.sendStatus(403);
+}
     
 });
 
